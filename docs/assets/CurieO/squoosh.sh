b8a3cebbd83dc0eba22d596eb407b61f7464a77a
Pacidus <pacidus@gmail.com>
2025-02-26 20:43:29 +0100

    trying to squoosh

--- docs/assets/CurieO/squoosh.sh
+++ docs/assets/CurieO/squoosh.sh
@@ -14,18 +14,5 @@ done
 
 for i in ./squoosh/*.png
 do
-     magick $i -channel RGB -separate -colors 256 -combine -depth 8 ${i%.*}.png
+    magick $i -colors 256 ${i%.*}.png
 done
-
-for f in {0..5}; do
-   for l in {0..9}; do
-      for s in {0..4}; do
-         outfile="out_${f}_${l}_${s}.png"
-         magick $i -define png:compression-filter=$f -define png:compression-level=$l -define png:compression-strategy=$s "$outfile"
-         size=$(du "$outfile")
-         echo filter:$f, level:$l, strategy:$s, size:$size
-      done
-   done
-done
-
-# magick mogrify -filter Triangle -define filter:support=2 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 20 -quality 50 -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB ./squoosh/*.png
